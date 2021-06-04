ac82da1046ccc5810984309c610ece684d25eba1
ishvara <thou.art.the.goatman@gmail.com>
2021-06-04 13:46:58 +0200

    3e commit

--- js/app.js
+++ js/app.js
@@ -17,3 +17,23 @@ anime.timeline({ loop: true })
         delay: 1000,
 
     });
+
+var textWrapper = document.querySelector('.ml4');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({ loop: true })
+    .add({
+        targets: '.ml4 .letter',
+        opacity: [0, 1],
+        easing: "easeInOutQuad",
+        duration: 3650,
+        delay: (el, i) => 150 * (i + 1)
+    }).add({
+        targets: '.ml4',
+        translateY: [0, 0],
+        opacity: [1, 0],
+        duration: 999999999999999999,
+        easing: "easeOutExpo",
+        delay: 1000,
+
+    });
